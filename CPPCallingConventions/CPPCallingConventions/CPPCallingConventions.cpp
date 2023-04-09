0eb915671e0ddde887bc22d48110b29027c40472
Efrain1096 <efrainmiguel@live.com>
2023-04-08 22:34:35 -0700

    Finished cop-ehh... implementing code for assembly and C++. Works as expected. Added comments as well to help facilitate code readibility.

--- CPPCallingConventions/CPPCallingConventions/CPPCallingConventions.cpp
+++ CPPCallingConventions/CPPCallingConventions/CPPCallingConventions.cpp
@@ -7,6 +7,14 @@ extern "C" void CalculateSum(int a, int b, int c, int *s1, int *s2, int *s3);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a = 1, b = 2, c = 3;
+	int s1, s2, s3;
+	
+	CalculateSum(a, b, c, &s1, &s2, &s3);
+	printf("Input a: %4d b: %4d c: %4d\n", a, b, c);
+	printf("Output sum: %4d square: %4d cube: %4d\n", s1, s2, s3);
+
+	return 0;
+    
 }
 
--- CPPCallingConventions/CPPCallingConventions/CalculateSum.asm
+++ CPPCallingConventions/CPPCallingConventions/CalculateSum.asm
@@ -15,7 +15,57 @@ CalculateSum	proc
 
 		push ebp;
 		mov ebp, esp
-		sub esp, 12
+		sub esp, 12 ; Allocate local storage space.
 		push ebx
 		push esi
 		push edi
+
+; Load arguments
+		mov eax, [ebp + 8]
+		mov ebx, [ebp + 12]
+		mov ecx, [ebp + 16]
+		mov	edx, [ebp + 20]
+		mov	esi, [ebp + 24]
+		mov	edi, [ebp + 28]
+
+; Compute s1 = a + b + c
+		mov [ebp - 12], eax
+		add [ebp - 12], ebx
+		add [ebp - 12], ecx
+
+; Compute s2 = a*a + b*b + c*c
+		imul eax, eax
+		imul ebx, ebx
+		imul ecx, ecx
+		mov [ebp - 8], eax
+		add [ebp - 8], ebx
+		add [ebp - 8], ecx
+
+; Compute s3 = a*a*a + b*b*b + c*c*c
+		imul eax, [ebp + 8]
+		imul ebx, [ebp + 12]
+		imul ecx, [ebp + 16]
+		mov	[ebp - 4], eax
+		add	[ebp - 4], ebx
+		add [ebp - 4], ecx
+
+; Save s1, s2, s3
+		mov eax, [ebp - 12]
+		mov [edx], eax
+		mov eax, [ebp - 8]
+		mov [esi], eax
+		mov eax, [ebp - 4]
+		mov [edi], eax
+
+; Epilogue. Restores the non-volatile registers.
+		pop edi
+		pop	esi
+		pop	ebx
+		mov	esp, ebp
+		pop ebp
+
+		ret
+
+
+CalculateSum	endp
+				end
