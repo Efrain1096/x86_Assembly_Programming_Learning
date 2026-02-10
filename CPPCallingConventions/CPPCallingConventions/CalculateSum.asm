3dd988d0a8d617e220e695c7343600b5675463ab
Efrain1096 <efrainmiguel@live.com>
2026-02-09 22:33:55 -0800

    I changed the Windows target platform version. Only to realize the IDE was set to x64 instead of the required x86 mode... oops. It's working fine, though.

--- CPPCallingConventions/CPPCallingConventions/CalculateSum.asm
+++ CPPCallingConventions/CPPCallingConventions/CalculateSum.asm
@@ -10,12 +10,12 @@ CalculateSum	proc
 		
 		; The procedure begins with a stack frame pointer initialization, along with the preservation of non-volatile registers on the stack.
 		; Before the registers are saved, subtract 12 from the esp register for allocating 12 bytes of local storage space that can be used by the function.
-		; The reason a subtraction is mad instead of adding is because the x86 stack grows downwards, towards lower memory addresses. 
+		; The reason a subtraction is made instead of adding is because the x86 stack grows downwards, towards lower memory addresses. 
 		; When using the ebp register to access values on the stack, function arguments are accessed using positive displacement of the stack. Whereas negative is for variables.
 
-		push ebp;
-		mov ebp, esp
-		sub esp, 12 ; Allocate local storage space.
+		push ebp ; Save the caller's base pointer onto the stack
+		mov ebp, esp ; Set the new base pointer to the current top of the stack
+		sub esp, 12 ; Allocate 12 bytes for variables in local storage space.
 		push ebx
 		push esi
 		push edi
