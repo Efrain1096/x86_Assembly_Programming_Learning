73422299b45e1cdf73f1a2d05433e21c93de309d
Efrain1096 <efrainmiguel@live.com>
2026-04-10 23:06:51 -0700

    It was a short but interesting example on adding up the values in CPP and seeing the ASM equivalent.

--- ArrayElementsAccess/ArrayElementsAccess/ArrayElementsAccess.cpp
+++ ArrayElementsAccess/ArrayElementsAccess/ArrayElementsAccess.cpp
@@ -3,7 +3,37 @@
 
 #include <iostream>
 
+
+// Remember to do function prototypes.
+
+extern "C" int CalcArraySumASM(int *x, int n);
+
+
+int CalcArraySumCPP(int *x, int n)
+{
+
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += *x++;
+	}
+
+	return sum;
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int index = 10;
+	int x[] = { 1,3,5,7,9,11 };
+	int n = sizeof(x) / sizeof(int);
+	int assembly_sum, cpp_sum;
+	assembly_sum = CalcArraySumASM(x, n);
+	cpp_sum = CalcArraySumCPP(x, n);
+
+	printf("Assembly sum = %d\n", assembly_sum);
+	printf("C++ sum = %d\n", cpp_sum);
+
+	return 0;
+
 }
